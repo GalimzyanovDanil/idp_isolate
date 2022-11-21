2620ced7f8acb5a4901f6364c77550c5b5174b30
Danil Galimzyanov <86564275+GalimzyanovDanil@users.noreply.github.com>
2022-11-21 08:31:25 +0300

    impl

---
+++ lib/model.dart
@@ -0,0 +1,55 @@
+import 'dart:convert';
+
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:json_annotation/json_annotation.dart';
+
+part 'model.g.dart';
+
+/// Модель станции в справочнике/API
+@immutable
+@JsonSerializable()
+class StationModel {
+  /// Код станции
+  @JsonKey(name: 'Code')
+  final int code;
+
+  /// Код-6 станции
+  @JsonKey(name: 'Code 6')
+  final String code6;
+
+  /// Наименование станции
+  @JsonKey(name: 'Name')
+  final String name;
+
+  /// Дефолтный конструктор
+  const StationModel({
+    required this.code,
+    required this.code6,
+    required this.name,
+  });
+
+  /// @nodoc
+  factory StationModel.fromJson(Map<String, dynamic> json) {
+    return _$StationModelFromJson(json);
+  }
+
+  @override
+  String toString() => 'Station model: $name';
+
+  /// @nodoc
+  Map<String, dynamic> toJson() => _$StationModelToJson(this);
+}
+
+/// Декодирование JSON в модель
+Future<int> fetchData(String dictionaryStr) async {
+  final decodeResult = json.decode(dictionaryStr) as List<dynamic>;
+
+  final l = decodeResult
+      .map<StationModel>(
+        (dynamic e) => StationModel.fromJson(e as Map<String, dynamic>),
+      )
+      .length;
+
+  return l;
+}
---
+++ lib/model.g.dart
@@ -0,0 +1,20 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'model.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+StationModel _$StationModelFromJson(Map<String, dynamic> json) => StationModel(
+      code: json['Code'] as int,
+      code6: json['Code 6'] as String,
+      name: json['Name'] as String,
+    );
+
+Map<String, dynamic> _$StationModelToJson(StationModel instance) =>
+    <String, dynamic>{
+      'Code': instance.code,
+      'Code 6': instance.code6,
+      'Name': instance.name,
+    };
